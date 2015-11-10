574a3ddd2dba0c1f4f3a35b3a092d97d866c7b14
zhangguixu <1447905210@qq.com>
2015-11-10 16:58:55 +0800

    对接开始

---
+++ Application/Home/Controller/AppController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangguixu
+ * Date: 2015/11/10
+ * Time: 16:55
+ */
+
+namespace Home\Controller;
+
+use Think\Controller;
+class AppController extends  Controller
+{
+    public function application(){
+        $this->display();
+    }
+
+}
