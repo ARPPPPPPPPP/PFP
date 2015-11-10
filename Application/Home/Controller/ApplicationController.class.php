d78f650919c51c51431a0b8eab5845be5786a5fe
zhangguixu <1447905210@qq.com>
2015-11-10 17:25:35 +0800

    对接开始

--- Application/Home/Controller/ApplicationController.class.php
+++ Application/Home/Controller/ApplicationController.class.php
@@ -5,14 +5,11 @@
  * Date: 2015/11/10
  * Time: 16:55
  */
-
 namespace Home\Controller;
-
 use Think\Controller;
-class AppController extends  Controller
+class ApplicationController extends  Controller
 {
     public function application(){
         $this->display();
     }
-
 }
