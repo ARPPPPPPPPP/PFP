dfa47e12adf5a8c0c4c67f37a08c1672eded7d3a
zhangguixu <1447905210@qq.com>
2015-11-11 01:54:08 +0800

    基本完成

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -11,6 +11,22 @@ use Think\Controller;
 class IndexController extends  Controller
 {
     public function index(){
+        $homePicutre = M ( 'homepicture' );
+        $news=M('breakingnews');
+        //背景图
+        $orderby ['homepictureid'] = 'desc';
+        $list = $homePicutre->order ( $orderby )->limit (1)->select ();
+
+        $newsorderby['breakingnewsid']='desc';
+        //学生之声
+        $voiceslist=$news->order($newsorderby)->where('breakingnewstype=10')->limit(4)->select();
+
+        //新闻
+        $newslist=$news->order($newsorderby)->where('breakingnewstype=1')->limit(2)->select();
+
+        $this->assign('bgpic',$list[0]);
+        $this->assign('voices',$voiceslist);
+        $this->assign('news',$newslist);
         $this->display();
     }
 
